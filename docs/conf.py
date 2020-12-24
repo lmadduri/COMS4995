2b5a16547b5a0b7230b298933f3c705358c41ed0
ilianac <idc2117@columbia.edu>
2020-12-24 17:32:06 -0500

    added read the docs

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,7 @@ author = 'Iliana Cantu, Lalitha Madduri'
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
