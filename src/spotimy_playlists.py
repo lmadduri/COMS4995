795c12bf089483d992201bd0d709fe07867bff50
ilianac <idc2117@columbia.edu>
2020-12-24 03:20:28 -0500

    added flake8 linting and fixed errors to test

--- src/spotimy_playlists.py
+++ src/spotimy_playlists.py
@@ -3,5 +3,6 @@ import random
 
 class Playlists(object):
     num = random.random()
+
     def get_num(self):
-    	return self.num
+        return self.num
--- tests/playlists_test.py
+++ tests/playlists_test.py
@@ -1,7 +1,8 @@
 import src.spotimy_playlists as sp
 
+
 def test_get_num():
 
-	x = sp.Playlists()
-	num = x.get_num()
-	assert (num >=0) and (num < 1)
+    x = sp.Playlists()
+    num = x.get_num()
+    assert (num >= 0) and (num < 1)
